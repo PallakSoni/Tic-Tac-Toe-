c4ac02f81673e26b39dee9663fd4921ca2741671
PallakSoni <91110506+PallakSoni@users.noreply.github.com>
2021-10-06 14:30:17 -0400

    Add files via upload

---
+++ TicTacToe.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace TicTacToe
+{
+    partial class TicTacToe
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlMain = new System.Windows.Forms.Panel();
+            this.ptb10 = new System.Windows.Forms.PictureBox();
+            this.ptb20 = new System.Windows.Forms.PictureBox();
+            this.ptb01 = new System.Windows.Forms.PictureBox();
+            this.ptb02 = new System.Windows.Forms.PictureBox();
+            this.ptb21 = new System.Windows.Forms.PictureBox();
+            this.ptb22 = new System.Windows.Forms.PictureBox();
+            this.ptb12 = new System.Windows.Forms.PictureBox();
+            this.ptb11 = new System.Windows.Forms.PictureBox();
+            this.ptb00 = new System.Windows.Forms.PictureBox();
+            this.pnlMain.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ptb10)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptb20)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptb01)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptb02)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptb21)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptb22)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptb12)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptb11)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptb00)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnlMain
+            // 
+            this.pnlMain.BackColor = System.Drawing.SystemColors.Control;
+            this.pnlMain.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlMain.Controls.Add(this.ptb10);
+            this.pnlMain.Controls.Add(this.ptb20);
+            this.pnlMain.Controls.Add(this.ptb01);
+            this.pnlMain.Controls.Add(this.ptb02);
+            this.pnlMain.Controls.Add(this.ptb21);
+            this.pnlMain.Controls.Add(this.ptb22);
+            this.pnlMain.Controls.Add(this.ptb12);
+            this.pnlMain.Controls.Add(this.ptb11);
+            this.pnlMain.Controls.Add(this.ptb00);
+            this.pnlMain.Location = new System.Drawing.Point(51, 22);
+            this.pnlMain.Name = "pnlMain";
+            this.pnlMain.Size = new System.Drawing.Size(363, 364);
+            this.pnlMain.TabIndex = 0;
+            // 
+            // ptb10
+            // 
+            this.ptb10.Image = global::TicTacToe.Properties.Resources.Initial_Border;
+            this.ptb10.Location = new System.Drawing.Point(15, 114);
+            this.ptb10.Name = "ptb10";
+            this.ptb10.Size = new System.Drawing.Size(100, 108);
+            this.ptb10.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ptb10.TabIndex = 8;
+            this.ptb10.TabStop = false;
+            this.ptb10.Click += new System.EventHandler(this.ptb10_Click);
+            // 
+            // ptb20
+            // 
+            this.ptb20.Image = global::TicTacToe.Properties.Resources.Initial_Border;
+            this.ptb20.Location = new System.Drawing.Point(15, 228);
+            this.ptb20.Name = "ptb20";
+            this.ptb20.Size = new System.Drawing.Size(100, 108);
+            this.ptb20.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ptb20.TabIndex = 7;
+            this.ptb20.TabStop = false;
+            this.ptb20.Click += new System.EventHandler(this.ptb20_Click);
+            // 
+            // ptb01
+            // 
+            this.ptb01.Image = global::TicTacToe.Properties.Resources.Initial_Border;
+            this.ptb01.Location = new System.Drawing.Point(121, 3);
+            this.ptb01.Name = "ptb01";
+            this.ptb01.Size = new System.Drawing.Size(100, 108);
+            this.ptb01.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ptb01.TabIndex = 6;
+            this.ptb01.TabStop = false;
+            this.ptb01.Click += new System.EventHandler(this.ptb01_Click);
+            // 
+            // ptb02
+            // 
+            this.ptb02.Image = global::TicTacToe.Properties.Resources.Initial_Border;
+            this.ptb02.Location = new System.Drawing.Point(227, 3);
+            this.ptb02.Name = "ptb02";
+            this.ptb02.Size = new System.Drawing.Size(100, 108);
+            this.ptb02.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ptb02.TabIndex = 5;
+            this.ptb02.TabStop = false;
+            this.ptb02.Click += new System.EventHandler(this.ptb02_Click);
+            // 
+            // ptb21
+            // 
+            this.ptb21.Image = global::TicTacToe.Properties.Resources.Initial_Border;
+            this.ptb21.Location = new System.Drawing.Point(121, 228);
+            this.ptb21.Name = "ptb21";
+            this.ptb21.Size = new System.Drawing.Size(100, 108);
+            this.ptb21.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ptb21.TabIndex = 4;
+            this.ptb21.TabStop = false;
+            this.ptb21.Click += new System.EventHandler(this.ptb21_Click);
+            // 
+            // ptb22
+            // 
+            this.ptb22.Image = global::TicTacToe.Properties.Resources.Initial_Border;
+            this.ptb22.Location = new System.Drawing.Point(227, 228);
+            this.ptb22.Name = "ptb22";
+            this.ptb22.Size = new System.Drawing.Size(100, 108);
+            this.ptb22.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ptb22.TabIndex = 3;
+            this.ptb22.TabStop = false;
+            this.ptb22.Click += new System.EventHandler(this.ptb22_Click);
+            // 
+            // ptb12
+            // 
+            this.ptb12.Image = global::TicTacToe.Properties.Resources.Initial_Border;
+            this.ptb12.Location = new System.Drawing.Point(227, 114);
+            this.ptb12.Name = "ptb12";
+            this.ptb12.Size = new System.Drawing.Size(100, 108);
+            this.ptb12.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ptb12.TabIndex = 2;
+            this.ptb12.TabStop = false;
+            this.ptb12.Click += new System.EventHandler(this.ptb12_Click);
+            // 
+            // ptb11
+            // 
+            this.ptb11.Image = global::TicTacToe.Properties.Resources.Initial_Border;
+            this.ptb11.Location = new System.Drawing.Point(121, 114);
+            this.ptb11.Name = "ptb11";
+            this.ptb11.Size = new System.Drawing.Size(100, 108);
+            this.ptb11.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ptb11.TabIndex = 1;
+            this.ptb11.TabStop = false;
+            this.ptb11.Click += new System.EventHandler(this.ptb11_Click);
+            // 
+            // ptb00
+            // 
+            this.ptb00.Image = global::TicTacToe.Properties.Resources.Initial_Border;
+            this.ptb00.Location = new System.Drawing.Point(15, 3);
+            this.ptb00.Name = "ptb00";
+            this.ptb00.Size = new System.Drawing.Size(100, 108);
+            this.ptb00.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ptb00.TabIndex = 0;
+            this.ptb00.TabStop = false;
+            this.ptb00.Click += new System.EventHandler(this.ptb00_Click);
+            // 
+            // TicTacToe
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(511, 407);
+            this.Controls.Add(this.pnlMain);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "TicTacToe";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TicTacToe";
+            this.pnlMain.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.ptb10)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptb20)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptb01)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptb02)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptb21)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptb22)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptb12)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptb11)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptb00)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlMain;
+        private System.Windows.Forms.PictureBox ptb00;
+        private System.Windows.Forms.PictureBox ptb10;
+        private System.Windows.Forms.PictureBox ptb20;
+        private System.Windows.Forms.PictureBox ptb01;
+        private System.Windows.Forms.PictureBox ptb02;
+        private System.Windows.Forms.PictureBox ptb21;
+        private System.Windows.Forms.PictureBox ptb22;
+        private System.Windows.Forms.PictureBox ptb12;
+        private System.Windows.Forms.PictureBox ptb11;
+    }
+}
+
---
+++ TicTacToe.cs
@@ -0,0 +1,288 @@
+﻿/*
+ * 
+ * Assignment-1 
+ * 
+ * Created By: Palak Soni
+ * 
+ * 
+ */
+ using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacToe
+{
+    public partial class TicTacToe : Form
+    {
+        Image X = Properties.Resources.X_image;
+        Image O = Properties.Resources.O_IMAGE;
+        Image initial = Properties.Resources.Initial_Border;
+        int[,] arr = new int[3, 3];
+        int tieCount = 0;
+        
+
+        bool player1 = true; 
+        public TicTacToe()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Method for initial image
+        /// Game starts with 'X'
+        /// </summary>
+        /// <param name="picture"></param>
+        private void ImageShow(PictureBox picture)
+        {
+            if (player1 == true)
+            {
+                picture.Image = X;
+                player1 = false;
+            }
+            else
+            {
+                picture.Image = O;
+                player1 = true;
+            }
+            tieCount++;
+            picture.Enabled = false;
+            
+        }
+
+        /// <summary>
+        /// Button click event for every button to show image
+        /// And checking result
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+
+        private void ptb00_Click(object sender, EventArgs e)
+        {
+            ImageShow(ptb00);
+            CheckResult();
+        }
+
+        private void ptb01_Click(object sender, EventArgs e)
+        {
+            ImageShow(ptb01);
+            CheckResult();
+        }
+
+        private void ptb02_Click(object sender, EventArgs e)
+        {
+            ImageShow(ptb02);
+            CheckResult();
+        }
+
+        private void ptb10_Click(object sender, EventArgs e)
+        {
+            ImageShow(ptb10);
+            CheckResult();
+        }
+
+        private void ptb11_Click(object sender, EventArgs e)
+        {
+            ImageShow(ptb11);
+            CheckResult();
+        }
+
+        private void ptb12_Click(object sender, EventArgs e)
+        {
+            ImageShow(ptb12);
+            CheckResult();
+        }
+
+        private void ptb20_Click(object sender, EventArgs e)
+        {
+            ImageShow(ptb20);
+            CheckResult();
+        }
+
+        private void ptb21_Click(object sender, EventArgs e)
+        {
+            ImageShow(ptb21);
+            CheckResult();
+        }
+
+        private void ptb22_Click(object sender, EventArgs e)
+        {
+            ImageShow(ptb22);
+            CheckResult();
+        }
+
+        /// <summary>
+        /// Checking result with the position
+        /// By Rows, Columns, Diagonally
+        /// </summary>
+
+        public void CheckResult()
+        {
+            PictureBox picture = new PictureBox();
+            picture.Image = initial;
+          
+            // ROWS CHECK
+
+            if (ptb00.Image== X && ptb01.Image==X && ptb02.Image==X)
+            {
+              var result= MessageBox.Show("Yaay!! X Wins.","Tic Tac Toe",MessageBoxButtons.OK,MessageBoxIcon.Information);
+                if (result==DialogResult.OK)
+                {
+                    RestartGame();
+                }
+              
+            }
+            else if (ptb00.Image == O && ptb01.Image == O && ptb02.Image == O)
+            {
+                var result = MessageBox.Show("Yaay!! O Wins.", "Tic Tac Toe", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                if (result == DialogResult.OK)
+                {
+                    RestartGame();
+                }
+            }
+            else if (ptb10.Image == O && ptb11.Image == O && ptb12.Image == O)
+            {
+                var result = MessageBox.Show("Yaay!! O Wins.", "Tic Tac Toe", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                if (result == DialogResult.OK)
+                {
+                    RestartGame();
+                }
+            }
+            else if (ptb10.Image == X && ptb11.Image == X && ptb12.Image == X)
+            {
+                var result = MessageBox.Show("Yaay!! X Wins.", "Tic Tac Toe", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                if (result == DialogResult.OK)
+                {
+                    RestartGame();
+                }
+            }
+            else if (ptb20.Image == O && ptb21.Image == O && ptb22.Image == O)
+            {
+                var result = MessageBox.Show("Yaay!! O Wins.", "Tic Tac Toe", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+            else if (ptb20.Image == X && ptb21.Image == X && ptb22.Image == X)
+            {
+                var result = MessageBox.Show("Yaay!! X Wins.", "Tic Tac Toe", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                if (result == DialogResult.OK)
+                {
+                    RestartGame();
+
+                }
+            }
+
+            // DIAGONAL CHECK
+
+            else if (ptb00.Image == O && ptb11.Image == O && ptb22.Image == O)
+            {
+                var result = MessageBox.Show("Yaay!! O Wins.", "Tic Tac Toe", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                if (result == DialogResult.OK)
+                {
+                    RestartGame();
+                }
+            }
+            else if (ptb00.Image == X && ptb11.Image == X && ptb22.Image == X)
+            {
+                var result = MessageBox.Show("Yaay!! X Wins.", "Tic Tac Toe", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                if (result == DialogResult.OK)
+                {
+                    RestartGame();
+                }
+            }
+            else if (ptb02.Image == X && ptb11.Image == X && ptb20.Image == X)
+            {
+                var result = MessageBox.Show("Yaay!! X Wins.", "Tic Tac Toe", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                if (result == DialogResult.OK)
+                {
+                    RestartGame();
+                }
+            }
+            else if (ptb02.Image == O && ptb11.Image == O && ptb20.Image == O)
+            {
+                var result = MessageBox.Show("Yaay!! O Wins.", "Tic Tac Toe", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                if (result == DialogResult.OK)
+                {
+                    RestartGame();
+                }
+            }
+
+            //COLUMN CHECK
+
+            else if (ptb00.Image == O && ptb10.Image == O && ptb20.Image == O)
+            {
+                var result = MessageBox.Show("Yaay!! O Wins.", "Tic Tac Toe", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                if (result == DialogResult.OK)
+                {
+                    RestartGame();
+                }
+            }
+            else if (ptb00.Image == X && ptb10.Image == X && ptb20.Image == X)
+            {
+                var result = MessageBox.Show("Yaay!! X Wins.", "Tic Tac Toe", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                if (result == DialogResult.OK)
+                {
+                    RestartGame();
+                }
+            }
+            else if (ptb01.Image == X && ptb11.Image == X && ptb21.Image == X)
+            {
+                var result = MessageBox.Show("Yaay!! X Wins.", "Tic Tac Toe", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                if (result == DialogResult.OK)
+                {
+                    RestartGame();
+                }
+            }
+            else if (ptb01.Image == O && ptb11.Image == O && ptb21.Image == O)
+            {
+                var result = MessageBox.Show("Yaay!! O Wins.", "Tic Tac Toe", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                if (result == DialogResult.OK)
+                {
+                    RestartGame();
+                }
+            }
+            else if (ptb02.Image == X && ptb12.Image == X && ptb22.Image == X)
+            {
+                var result = MessageBox.Show("Yaay!! X Wins.", "Tic Tac Toe", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                if (result == DialogResult.OK)
+                {
+                    RestartGame();
+                }
+            }
+            else if (ptb02.Image == O && ptb12.Image == O && ptb22.Image == O)
+            {
+                var result = MessageBox.Show("Yaay!! O Wins.", "Tic Tac Toe", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                if (result == DialogResult.OK)
+                {
+                    RestartGame();
+                }
+            }
+
+            // Draw
+            else if (tieCount==9)
+            {
+                var result = MessageBox.Show("Its a Draw", "Tic Tac Toe", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                if (result == DialogResult.OK)
+                {
+                    RestartGame();
+                }
+            }
+        }
+
+        public void RestartGame()
+        {
+
+           
+    
+            ptb00 = ptb01 = ptb02=
+                ptb10 = ptb11 = ptb12
+                = ptb20 = ptb21= ptb22= null;
+
+
+        }
+    }
+}
